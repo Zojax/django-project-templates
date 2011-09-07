4caff3f9cbf080a8a01c5c250bfbf66e3ea70dcc
Anatoly Bubenkov <bubenkoff@gmail.com>
2011-09-07 19:23:39 +0300

    initial

--- src/django_project_templates/templates/buildout_django_project/project/views.py
+++ src/django_project_templates/templates/buildout_django_project/project/views.py
@@ -6,42 +6,9 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-#from registration import signals
-from pages.views import details
-from vdr.views import plans
-
-def registration_activation_complete_callback(sender, **kwargs):
-    user = kwargs['user']
-    request = kwargs['request']
-    backend = ModelBackend
-    user.backend = 'django.contrib.auth.backends.ModelBackend'
-    login(request, user)
-#signals.user_activated.connect(registration_activation_complete_callback)
-
-
-@login_required
-def post_login_handler(request):
-
-    user = request.user
-
-    try:
-        profile_obj = user.get_profile()
-    except ObjectDoesNotExist:
-        return HttpResponseRedirect(reverse("profiles_create_profile"))
-
-    if not user.email or not user.first_name or not user.last_name:
-        return HttpResponseRedirect(reverse("profiles_complete_profile"))
-
-    if profile_obj.location is None or not profile_obj.location.city:
-        return HttpResponseRedirect(reverse("profiles_complete_profile"))
-
-    return HttpResponseRedirect("/")
-
 
 def index_view(request):
-    if request.user.is_authenticated():
-        return HttpResponseRedirect(reverse("browse-folder"))
-    return plans(request)
+    return render_to_response('templates/base.html')
 
 def handler500(request, template_name='500.html'):
     """
